ad004f5d243e9f1b77eee85b3a63bfee096c3516
almud5044 <almud5044@oslomet.no>
2025-10-22 11:31:33 +0200

    adds models

---
+++ api/Models/Quiz.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+namespace ITPE3200FAM.Models;
+
+
+public class Quiz
+{
+    public int QuizId { get; set; }
+
+    [Required(ErrorMessage = "The Title is required.")]
+    [StringLength(100, MinimumLength = 2, ErrorMessage = "The Title must be between 2 and 100 characters.")]
+    [Display(Name = "Quiz Title")]
+    public string Title { get; set; } = string.Empty;
+    public List<Question> Questions { get; set; } = new();
+}
