de8e7b7a7b424fae2ba551dce107d3c9e3f57a62
faver3645 <143396926+faver3645@users.noreply.github.com>
2025-10-29 16:51:13 +0100

    commit

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfc352a541e02bdd7507d7330b8af561e77ffc2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80fb6f01cad9f3b5f52a216515bde766a446bb38")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
