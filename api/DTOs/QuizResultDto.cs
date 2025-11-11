53ecd3e692d23cc2c1c846162d33e7a9042ff4a5
faver3645 <143396926+faver3645@users.noreply.github.com>
2025-11-11 14:34:55 +0100

    fix

--- api/DTOs/QuizResultDto.cs
+++ api/DTOs/QuizResultDto.cs
@@ -5,9 +5,6 @@ namespace api.DTOs
     public class QuizResultDto
     {
         public int QuizResultId { get; set; }
-
-        [Required(ErrorMessage = "The name is required.")]
-        [StringLength(100, MinimumLength = 2, ErrorMessage = "The name must be between 2 and 100 characters.")]
         public string UserName { get; set; } = string.Empty;
         public int QuizId { get; set; }
         public string QuizTitle { get; set; } = string.Empty;
