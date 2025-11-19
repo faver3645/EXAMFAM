fc687c590f67ea507ace54ded5a0dae62869e72e
almud5044 <almud5044@oslomet.no>
2025-11-19 10:59:21 +0100

    moved a fil to Dto

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd947e5ca687b8ff30e354895e7e6b5b8dc1e6c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8332145631e47021c1d1baed76ef863fe72bfc18")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
