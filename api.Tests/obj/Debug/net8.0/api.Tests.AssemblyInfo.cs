7287e13ea8ab05138a14b6b68f106675493c228f
faver3645 <143396926+faver3645@users.noreply.github.com>
2025-11-26 14:56:52 +0100

    deleted test

--- api.Tests/obj/Debug/net8.0/api.Tests.AssemblyInfo.cs
+++ api.Tests/obj/Debug/net8.0/api.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75d26d12f1eceaf4b96976a7fb11d5600b7bf02f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4682b23b718c34895dfec13d4e9f37cfdf92fa64")]
 [assembly: System.Reflection.AssemblyProductAttribute("api.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2daa67468c5789cc9a99d0a562d571fe86cf429c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4682b23b718c34895dfec13d4e9f37cfdf92fa64")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
